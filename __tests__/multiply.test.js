560f1bc447b2fb10865535d85324664d1c571bff
mnyborg <nyborg62@gmail.com>
2025-08-10 15:04:22 +0200

    Multiply test fails

--- __tests__/multiply.test.js
+++ __tests__/multiply.test.js
@@ -6,9 +6,9 @@ test('multiply 1 and 2 to equal 2', () => {
 
 test('multiply multiple numbers', () => {
   expect(multiply(1, 2, 3)).toBe(6);
-  expect(multiply(10, -10, 5)).toBe(-500);
+  expect(multiply(10, -10, 5)).toBe(-100);
 });
 
-test('returns 0 when no arguments are passed', () => {
+test('returns 1 when no arguments are passed', () => {
   expect(multiply()).toBe(1);
 });
